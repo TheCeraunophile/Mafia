d0392b1b164ff86c703e7c46ae5ff25ae459470f
Soheil@DESKTOP-R71JFFO <shahrabi.soheil@gmail.com>
2021-03-24 18:59:15 +0430

    fix possible of compare between of objcts

--- Player.java
+++ Player.java
@@ -11,7 +11,16 @@ public class Player {
 
     @Override
     public String toString(){
-        String result =" The person : " + name + " Whit role of " + role;
+        String result =" The person : " + name + " Whit role of " + role+"  ";
         return result;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return name.equals(player.name);
+    }
+
 }
