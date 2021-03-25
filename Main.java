3140919c33de389af7670b98a8dbdfe6e8f6da1f
Soheil@DESKTOP-R71JFFO <shahrabi.soheil@gmail.com>
2021-03-25 16:57:55 +0430

    divide God into another two class

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+        public static void main(String[] args) {
+            Scanner scanner=new Scanner(System.in);
+            while (scanner.hasNext()) {
+            String act = scanner.next();
+            switch (act) {
+                case "create_game":
+                case "assign_role":
+                case "start_game":
+//                case "end_vote":
+                case "get_game_state":
+            }
+        }
+    }
+}
