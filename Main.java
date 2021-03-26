296605d9a4f37cd75cc0af5989045be9b573806e
Soheil@DESKTOP-R71JFFO <shahrabi.soheil@gmail.com>
2021-03-27 01:58:51 +0430

    update Main class

--- Main.java
+++ Main.java
@@ -3,13 +3,16 @@ public class Main {
         public static void main(String[] args) {
             Scanner scanner=new Scanner(System.in);
             while (scanner.hasNext()) {
-            String act = scanner.next();
-            switch (act) {
-                case "create_game":
-                case "assign_role":
-                case "start_game":
-//                case "end_vote":
-                case "get_game_state":
+            God god = God.getGod();
+            while (scanner.hasNext()) {
+                String act = scanner.next();
+                switch (act) {
+                    case "create_game" -> god.creatingGame();
+                    case "assign_role" -> god.assignRole();
+                    case "start_game" -> god.day();
+                    case "get_game_state" -> god.getGameStatus();
+                    default -> System.out.println("your command is undifined");
+                }
             }
         }
     }
