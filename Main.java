ac0ba68a314099a43ff4719c6bf4ae3b94fedb4d
Soheil@DESKTOP-R71JFFO <shahrabi.soheil@gmail.com>
2021-03-30 19:21:58 +0430

    cut some useless line in Main class

--- Main.java
+++ Main.java
@@ -1,18 +1,16 @@
 import java.util.Scanner;
 public class Main {
-        public static void main(String[] args) {
-            Scanner scanner=new Scanner(System.in);
-            while (scanner.hasNext()) {
-            God god = God.getGod();
-            while (scanner.hasNext()) {
-                String act = scanner.next();
-                switch (act) {
-                    case "create_game" -> god.creatingGame();
-                    case "assign_role" -> god.assignRole();
-                    case "start_game" -> god.day();
-                    case "get_game_state" -> god.getGameStatus();
-                    default -> System.out.println("your command is undifined");
-                }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        God god = God.getGod();
+        while (scanner.hasNext()) {
+            String act = scanner.next();
+            switch (act) {
+                case "create_game" -> god.creatingGame();
+                case "assign_role" -> god.assignRole();
+                case "start_game" -> god.day();
+                case "get_game_state" -> god.getGameStatus();
+                default -> System.out.println("your command is undifined");
             }
         }
     }
